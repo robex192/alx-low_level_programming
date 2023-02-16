d136a5020b6461869999944ec7e012f9d3df2791
robex192 <robakebeba@gmail.com>
2023-02-16 11:58:08 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
+#include<stdio.h>
+/**
+ * main -prints "Programming is like building a multilingual puzzle"
+ * return 0.
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    returns (0);
+}
 
