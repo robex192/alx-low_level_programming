852d6c82478b11b1acfa5b3a0e24ae21117b7a8f
robex192 <robakebeba@gmail.com>
2023-02-16 21:05:17 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,9 @@
 *Return: Always 0 (Success)
 */
 
+
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
-return(0);
+   puts("\"programming is like building a multilingual puzzle");
+   return(0);
 }
