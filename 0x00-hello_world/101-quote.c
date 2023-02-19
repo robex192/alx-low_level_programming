4d5fd358b8fc0751f716c71fd6d2abe96605fdfd
robex192 <robakebeba@gmail.com>
2023-02-18 23:00:41 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,8 @@
  * Return 1 (Success)
  */
 int main(void)
-`
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+{`
+        fput("and that piece of art is useful\" -   Dora Korpar, 2015-10-19\n", stdout);
+
+return (1);
+}
