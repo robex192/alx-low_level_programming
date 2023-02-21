d4a730db383313f61fd4891ccea4077252d9c946
robex192 <robakebeba@gmail.com>
2023-02-20 22:25:41 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,7 +5,7 @@
  *
  * Return: Always 0.
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	char ch;
 	for (ch = 'a'; ch <= 'z'; ch++)
@@ -16,6 +16,6 @@ void print_alphabet(void)
 }
 int main()
 {
-	print_alphabet();
+	print_alphabet_x10();
 }
 
