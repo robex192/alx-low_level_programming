05d2f58b22f2713928d0816fabec1c03b89d2b8d
robex192 <robakebeba@gmail.com>
2023-02-21 17:40:08 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,12 +9,9 @@
  */
 int _isalpha(int c)
 {
-
-	if (((c >= 97) && (c <= 122)) || ((c >= 65) && (c <= 90)))
-	
-		return(1);
-	
+	if ((c >= 'a' && c <= 'z') ||
+	    (c >= 'A' && c <= 'Z'))
+		return (1);
 	else
-	
-		return (0);	
+		return (0);
 }
