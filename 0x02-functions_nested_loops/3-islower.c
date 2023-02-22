d9b14c7debe1ed1fe1a9fbc5f00286696cd0b67a
robex192 <robakebeba@gmail.com>
2023-02-21 17:00:41 -0800

    1-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,12 @@
 
 /**
 *_islower - Check for lowercase character
-*c: character to be ckecked return 1 for lowercase character or 0 for anything else
+*c: character to be ckecked return 1 for 
+*lowercase character or 0 for anything else
 */
 
 int _islower(int c)
 {
-
 	if ((c >= 97) && (c <= 122))
 	{
 		return (1);
