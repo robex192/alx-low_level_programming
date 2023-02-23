2a708037a4e88611f3633187bcbd0beb863db174
robex192 <robakebeba@gmail.com>
2023-02-23 04:40:30 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,16 +6,11 @@
  *
  * Return: 1 if character is uppercase, 0 otherwise.
  */
-
 int _isupper(int c)
 {
+	if (c >= 65 && c <= 90)
+		return (1);
 
-	if (c <= 65 && c <= 90)
-	{
-		return(1);
-	}
 	else
-	{
-	return(0);
-	}
+		return (0);
 }
