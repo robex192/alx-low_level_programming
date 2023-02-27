32381014dc155ddf164702275db4cd437bad1b45
robex192 <robakebeba@gmail.com>
2023-02-27 08:51:59 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @strl: string
+ * Return: length
+ */
+int _strlen(char *strl)
+{
+	int leng = 0;
+
+	while (*strl != '\0')
+	{
+		leng++;
+		strl++;
+	}
+
+	return (leng++);
+}
