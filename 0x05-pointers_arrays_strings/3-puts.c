567c64089fead304729dcbba52688c05ded17d56
robex192 <robakebeba@gmail.com>
2023-02-27 10:45:04 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @strp: string to print
+ */
+void _puts(char *strp)
+{
+	while (*strp != '\0')
+	{
+		_putchar(*strp++);
+	}
+	_putchar('\n');
+}
