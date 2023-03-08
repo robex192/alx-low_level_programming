f8ac7d817ddb6edb6de0410d08ca65f011850966
robex192 <robakebeba@gmail.com>
2023-03-08 03:09:19 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,16 +7,13 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-
 void _puts_recursion(char *s)
 {
-
 	if (*s == 0)
 	{
 		_putchar('\n');
 		return;
 	}
 	_putchar(*s);
-	_puts_recursion(s+1);
-
+	_puts_recursion(s + 1);
 }
