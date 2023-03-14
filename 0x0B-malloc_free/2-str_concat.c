f39e4ebae5db66296669ef00c0bd1e096677b49a
robex192 <robakebeba@gmail.com>
2023-03-14 11:36:21 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,39 +8,42 @@
  * @s1: input one to concat
  * @s2: input two to concat
  * Return: concat of s1 and s2
- */
+*/
+
 char *str_concat(char *s1, char *s2)
 {
-	int a, b;
-	char *s;
+	char *conct;
+	int i, ci;
 
 	if (s1 == NULL)
-	{
+	
 		s1 = "";
-	}
+	
 	if (s2 == NULL)
 	{
 		s2 = "";
+		i = ci = 0;
 	}
-	for (a = 0; s1[a] != '\0'; a++)
+	while (s1[i] != '\0')
+		i++;
+	while (s2[ci] != '\0')
+		ci++;
+	conct = malloc(sizeof(char) * (i + ci + 1));
+	if (conct == NULL)
 	{
-	}
-	for (b = 0; s2[b] != '\0'; b++)
-	{
-	}
-
-	s = (char *)  malloc(((a + b) + 1) * sizeof(char));
-
-	if (s == NULL)
 		return (NULL);
-	for (a = 0; s1[a] != '\0'; a++)
-	{
-		s[a] = s1[a];
+	i = ci = 0;
 	}
-	for (b = 0; s2[b] != '\0'; b++)
+	while (s1[i] != '\0')
 	{
-		s[a] = s2[b];
-		a++;
+		conct[i] = s1[i];
+		i++;
 	}
-	return (s);
-}	
+	while (s2[ci] != '\0')
+	{
+		conct[i] = s2[ci];
+		i++, ci++;
+	}
+	conct[i] = '\0';
+	return (conct);
+}
