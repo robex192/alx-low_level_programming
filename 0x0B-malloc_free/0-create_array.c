ebfca298e819ee133a5101ea54b5c77b7aa96d2e
robex192 <robakebeba@gmail.com>
2023-03-14 04:47:15 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -28,14 +28,11 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 		
 
-		
 	str = malloc(sizeof(char) * size);
 		
 	if (size == 0 || str == NULL)
 		
 		return (NULL);
-		
-
 		
 	for (i = 0; i < size; i++)
 		
