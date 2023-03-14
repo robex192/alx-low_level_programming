ae9b3795122f321ae07d2b29ac43265f1d5feff8
robex192 <robakebeba@gmail.com>
2023-03-14 12:32:38 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -8,6 +8,7 @@
  * @height: height input
  * Return: pointer to  dim. array		
  */
+
 int **alloc_grid(int width, int height)		
 {
 	int **mee;
@@ -32,10 +33,7 @@ int **alloc_grid(int width, int height)
 	for (x = 0; x < height; x++)	
 	{
 		for (y = 0; y < width; y++)
-		
 			mee[x][y] = 0;	
-	}		
-		
+	}				
 	return (mee);		
 }
-
