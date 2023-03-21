3bac12f95c959216739b699911a9725f8512457f
robex192 <robakebeba@gmail.com>
2023-03-21 08:37:06 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,9 +14,9 @@ int _strlen(char *s)
 		
 		while (s[i] != '\0')
 		{
-		i++;
+			i++;
 		}
-
+		
 		return (i);
 }
 
@@ -26,8 +26,8 @@ int _strlen(char *s)
 * to the buffer pointed to by dest
 * @dest: pointer to the buffer in which we copy the string
 * @src: string to be copied
-*
-* Return: the pointer to dest
+* 
+* Return:the pointer to dest
 */
 char *_strcpy(char *dest, char *src)
 {
@@ -59,10 +59,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 		dog_t *dog;
 		int len1, len2;
-		
+
 		len1 = _strlen(name);
 		len2 = _strlen(owner);
-		dog = malloc(sizeof(dog_t));
+ 		dog = malloc(sizeof(dog_t));
 		if (dog == NULL)
 			return (NULL);
 		dog->name = malloc(sizeof(char) * (len1 + 1));
@@ -81,5 +81,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		_strcpy(dog->name, name);
 		_strcpy(dog->owner, owner);
 		dog->age = age;
-			return (dog);
+
+		return (dog);
 }
