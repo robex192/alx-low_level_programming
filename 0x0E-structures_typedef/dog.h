c5870b12fcbd691ca43462c52887df2e005cf8e4
robex192 <robakebeba@gmail.com>
2023-03-21 04:36:46 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,12 +17,10 @@
 		char *owner;
 	};
 
-
 	/**
 	 * dog_t - typedef for struct dog
 	 */
 	typedef struct dog dog_t;
-
 
 	void init_dog(struct dog *d, char *name, float age, char *owner);
 	void print_dog(struct dog *d);
@@ -30,6 +28,5 @@
 	void free_dog(dog_t *d);
 	char *_strcpy(char *dest, char *src);
 	int _strlen(char *s);
-
 
 	#endif
